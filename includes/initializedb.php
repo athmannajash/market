2f1b15d7a9e191a2398f235de25d98f90f86ead3
Athmann <37657102+iamathman@users.noreply.github.com>
2018-11-29 11:30:48 -0800

    Add files via upload

---
+++ includes/initializedb.php
@@ -0,0 +1,9 @@
+
+<?php
+	$host="localhost";
+	$username="root";
+	$password="athman";
+	$dbname="market";
+	mysql_connect("$host","$username","$password");
+	mysql_select_db("$dbname");
+?>
---
+++ includes/navigation.php
@@ -0,0 +1,32 @@
+<body>
+<header>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+        <div class="container topnav">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="../index.php">market</a>
+            </div>
+
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="sell.php" class="btn btn-default" role="button">Sell</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+</header>
